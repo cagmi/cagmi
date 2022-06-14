841150958b7f45750dd327ed9c53024cad760292
Carlos Aguilar Miguel <cagmiteleco@gmail.com>
2022-06-14 16:21:48 +0200

    add static folder and base html

--- software/python/libraries/flask/to-do-list/app.py
+++ software/python/libraries/flask/to-do-list/app.py
@@ -1,6 +1,19 @@
+from email.policy import default
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"<Task {self.id}>"
 
 @app.route("/")
 def hello_world():
